c9a8d2b655bae59ec45b5d91bc61d55721a88f8d
Nada Sabry <Nada_201900894@fci.helwan.edu.eg>
2023-06-22 09:29:30 +0300

    route to sectioms

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  activenow: string = "hsl(176,71%,43%)";
+  active: string[] = [this.activenow, '', '', ''];
 
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.navigate(['']);
+  }
+  
+  standup(index: number) {
+    for (let i = 0; i < this.active.length; i++) {
+      if (i == index) {
+        this.active[i] = this.activenow;
+      }
+      else {
+        this.active[i] = '';
+      }
+    }
+  }
 }
