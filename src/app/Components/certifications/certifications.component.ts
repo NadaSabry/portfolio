b19c30f68e0f99e826085a47d3ee7101430ffb7f
Nada Sabry <Nada_201900894@fci.helwan.edu.eg>
2023-07-02 21:19:25 +0300

    add ngx-own

--- src/app/Components/certifications/certifications.component.ts
+++ src/app/Components/certifications/certifications.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-certifications',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./certifications.component.scss']
 })
 export class CertificationsComponent {
+  isVisible = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const myDiv = document.getElementById("certificate");
+    const height = myDiv?.offsetHeight;
+
+// Log the width and height to the console
+
+    const boxPosition = document.querySelector('#certificate')?.getBoundingClientRect().top;
+    const scrollPosition = window.innerHeight + window.pageYOffset;
+    //console.log("about " ,boxPosition , ", " ,scrollPosition);
+
+    if (boxPosition!= undefined && height!=undefined && boxPosition <= 350.0 && boxPosition >= -height) {
+      this.isVisible = true;
+    } else {
+      this.isVisible = false;
+    }
+  }
+  
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['1', '2'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      }
+    },
+    nav: true
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,10 +24,12 @@ import { CertificationsComponent } from './Components/certifications/certificati
     ContactComponent,
     CardComponent,
     CertificationsComponent
+    //SwiperModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
